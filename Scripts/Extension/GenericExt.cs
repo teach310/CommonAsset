7cddb6cd2c115f0a20cda83cba81ba0df3def433
Taichi Sato <okms310gmail.com>
2017-08-13 21:57:22 +0900

    GenericExt, CoroutineHandler

---
+++ Scripts/Extension/GenericExt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class GenericExt
+{
+
+    /// <summary>
+    /// オブジェクトが指定されたいずれかのオブジェクトと等しいかどうかを返します
+    /// </summary>
+    public static bool ContainsAny<T>(this T self, params T[] values)
+    {
+        return values.Any(c => c.Equals(self));
+    }
+}
